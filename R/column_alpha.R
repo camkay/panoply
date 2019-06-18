94f87b3b00b0bdd7a46efa13eb9195fa168a92af
Cameron Kay <ckay@uoregon.edu>
2019-06-18 11:06:55 -0700

    Implemented verbose in column alpha.

--- R/column_alpha.R
+++ R/column_alpha.R
@@ -4,6 +4,7 @@
 #' @param pattern a character string that is used to find the columns of interest. It can be a regular expression.
 #' @param data a data frame. 
 #' @param full if TRUE, the full reliability analysis produced by the `psych` package is returned. If FALSE, only the raw alpha value is returned.
+#' @param verbose specifies whether all column names should be listed, regardless of length. 
 #' @export
 #' @examples
 #' x <- data.frame(my_column_x  = 1:10,
@@ -17,7 +18,7 @@
 #' column_alpha(pattern = "x", data = x, full = FALSE)
 #' 
 
-column_alpha <- function(pattern, data, full = FALSE, na.rm = TRUE) {
+column_alpha <- function(pattern, data, full = FALSE, verbose = FALSE, na.rm = TRUE) {
   
   # check arguments
   argument_check(pattern, "pattern", "character", len_check = TRUE)
@@ -25,11 +26,11 @@ column_alpha <- function(pattern, data, full = FALSE, na.rm = TRUE) {
   argument_check(full, "full", "logical", len_check = TRUE)
   argument_check(na.rm, "na.rm", "logical", len_check = TRUE)
   
-  # drop columns not including the pattern
+  # find columns that match the pattern
   data_found <- column_find(pattern, data, return = "data.frame")
   
   # message user how the composites were created
-  column_messager(data_found, "Cronbach's Alpha")
+  column_message(data_found, "Cronbach's Alpha", verbose = verbose)
   
   # calculate and extract the alpha value
   alpha_out <- psych::alpha(x = data_found, na.rm = na.rm)
