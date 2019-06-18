0e09e3a449525b88718423eede49d5c266f5c220
Cameron Kay <ckay@uoregon.edu>
2019-06-18 11:04:52 -0700

    Updated column_message to allow for verbose messages.

--- R/column_message.R
+++ R/column_message.R
@@ -3,9 +3,10 @@
 #' A function for creating messages when an action is performed on the results of column_find. 
 #' @param x the results of column_find.
 #' @param function_operation the operation to be performed on the results of column_find. 
+#' @param verbose specifies whether all column names should be listed, regardless of length. 
 
 
-column_message <- function(data, function_operation) {
+column_message <- function(data, function_operation, verbose = FALSE) {
   
   # calculate number of columns
   num_cols <- ncol(data)
@@ -13,10 +14,10 @@ column_message <- function(data, function_operation) {
   # stop the function if no columns are returned or specify column names
   if (num_cols == 0) {
     stop("No columns matched the provided string.")
-  } else if (num_cols  <= 4) {
-    col_names <- paste(names(data), collapse = ", ")
-  } else {
+  } else if (num_cols > 4 & verbose == FALSE) {
     col_names <- paste0(paste(names(data)[1:3], collapse = ", "), ", and ", num_cols - 3, " more")
+  } else {
+    col_names <- paste(names(data), collapse = ", ")
   }
   
   message(paste0(function_operation,
