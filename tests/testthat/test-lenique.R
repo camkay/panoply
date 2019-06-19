7331996411a21d3247e457fb7fce4709001b9855
Cameron Kay <ckay@uoregon.edu>
2019-06-19 11:40:20 -0700

    Created test for column_find and renamed the test file for lenique.

--- tests/testthat/test-lenique.R
+++ tests/testthat/test-lenique.R
@@ -1,7 +1,7 @@
+# create example data
 char_example <- c("cat", "dog", "cat", "dog", "giraffe")
 
+# test
 test_that("lenique results are equal to length(unique(x)) results", {
   expect_equal(lenique(char_example), length(unique(char_example)))
 })
-
-
