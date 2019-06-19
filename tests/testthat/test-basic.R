295827eabc0fb8e49c836e65e60ae674e9355ac4
Cameron Kay <ckay@uoregon.edu>
2019-06-19 12:11:30 -0700

    Fixed problem missing length check.

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -1,10 +1,5 @@
-# create example data
+# create example character vector
 char_example <- c("cat", "dog", "cat", "dog", "giraffe")
-
-# test
-test_that("lenique results are equal to length(unique(x)) results", {
-  expect_equal(lenique(char_example), length(unique(char_example)))
-})
 
 # create example data
 data_example <- data.frame(scale1_item1 = c(6, 1, 3, 4, 5, 9, 9),
@@ -13,7 +8,12 @@ data_example <- data.frame(scale1_item1 = c(6, 1, 3, 4, 5, 9, 9),
                            scale2_item1 = c(9, 9, 9, 8, 4, 2, 2),
                            scale2_item2 = c(7, 8, 7, 9, 5, 1, 2))
 
-# test results
+# test lenique
+test_that("lenique results are equal to length(unique(x)) results", {
+  expect_equal(lenique(char_example), length(unique(char_example)))
+})
+
+# test column_find
 test_that("the correct parts of the data frame are returned", {
   expect_equal(column_find(pattern = "scale1", 
                            return  = "logical", 
@@ -36,11 +36,11 @@ test_that("the correct parts of the data frame are returned", {
 })
 
 # test errors
-test_that("lenique results are equal to length(unique(x)) results", {
-  expect_error(argument_check(300, "pattern", "character", len_check = TRUE))
-  expect_error(argument_check(c(12, 10), "pattern", "character", len_check = TRUE))
-  expect_error(argument_check("hello", "data", "data.frame"))
-  expect_error(argument_check(12, "data", "data.frame"))
-  expect_error(argument_check(12, "return", "character", len_check = TRUE))
-  expect_error(argument_check(c(12,10), "return", "character", len_check = TRUE))
+test_that("check errors", {
+  expect_error(argument_check(300, "x", "character", len_check = TRUE))
+  expect_error(argument_check(c("h", "i"), "x", "character", len_check = TRUE))
+  expect_error(argument_check("hello", "x", "data.frame"))
+  expect_error(argument_check(12, "x", "data.frame"))
+  expect_error(argument_check(12, "x", "character", len_check = TRUE))
+  expect_error(argument_check(c(12, 10), "x", "numeric", len_check = TRUE))
 })
