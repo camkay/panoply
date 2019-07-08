c2dc9c9b642c9c65194fb7a8ad2050f0503b14c2
Cameron Kay <ckay@uoregon.edu>
2019-07-08 12:24:35 -0700

    Added invert functionality to column_find.

--- R/column_find.R
+++ R/column_find.R
@@ -4,6 +4,7 @@
 #' @param pattern a character string that is used to find the columns of interest. It can be a regular expression.
 #' @param data a data frame. 
 #' @param return a string specifying the output desired. "logical" (the default) returns a logical vector with TRUE indicating that a column name includes the pattern. "numeric" returns a numeric vector that identifies the column numbers matching the pattern. "character" returns a character vector with the column names that match the pattern. "data.frame" returns a data frame that includes only those columns whose names match the pattern.
+#' @param invert if TRUE, identifies or extracts columns that DO NOT match the pattern.
 #' @export
 #' @examples
 #' x <- data.frame(my_column_x  = 1:10,
@@ -19,29 +20,38 @@
 #' column_find(pattern = "x", data = x, return = "data.frame")
 #' 
 
-column_find <- function(pattern, data, return = "logical") {
+column_find <- function(pattern, data, return = "logical", invert = FALSE) {
   
   # check arguments
   argument_check(pattern, "pattern", "character", len_check = TRUE)
   argument_check(data, "data", "data.frame")
   argument_check(return, "return", "character", len_check = TRUE)
+  argument_check(invert, "invert", "logical", len_check = TRUE)
   
   # format x for output
   if (return == "logical") {
-    out <- grepl(pattern, names(data))
+    if(invert == FALSE) {
+      out <- grepl(pattern, names(data))
+    } else {
+      out <- !grepl(pattern, names(data))
+    }
   } else if (return == "numeric") {
-    out <- grep(pattern, names(data))
+    out <- grep(pattern, names(data), invert = invert)
   } else if (return == "character" ) {
-    out <- grep(pattern, names(data), value = TRUE)
+    out <- grep(pattern, names(data), value = TRUE, invert = invert)
   } else if (return == "data.frame") {
-    out <- data[, grep(pattern, names(data))]
+    out <- data[, grep(pattern, names(data), invert = invert)]
   } else {
     warning(paste0(return, 
                    " is not a recognized return type. ",
                    "\"logical\" used instead. \n", 
                    "Other return options: ",
                    "\"numeric\", \"character\", and \"data.frame\"."))
-    out <- grepl(pattern, names(data))
+    if(invert == FALSE) {
+      out <- grepl(pattern, names(data))
+    } else {
+      out <- !grepl(pattern, names(data))
+    }
   }
   
   # return out
--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -40,19 +40,40 @@ test_that("column_find returns the correct parts of the data frame", {
                            data    = data_example), 
                c(TRUE, TRUE, TRUE, FALSE, FALSE))
   expect_equal(column_find(pattern = "scale1", 
+                           return  = "logical", 
+                           data    = data_example,
+                           invert  = TRUE), 
+               c(FALSE, FALSE, FALSE, TRUE, TRUE))
+  expect_equal(column_find(pattern = "scale1", 
                            return  = "numeric", 
                            data    = data_example), 
                c(1, 2, 3))
   expect_equal(column_find(pattern = "scale1", 
+                           return  = "numeric", 
+                           data    = data_example,
+                           invert  = TRUE), 
+               c(4, 5))
+  expect_equal(column_find(pattern = "scale1", 
                            return  = "character", 
                            data    = data_example), 
                c("scale1_item1", "scale1_item2", "scale1_item3"))
+  expect_equal(column_find(pattern = "scale1", 
+                           return  = "character", 
+                           data    = data_example,
+                           invert  = TRUE), 
+               c("scale2_item1", "scale2_item2"))
   expect_equal(column_find(pattern = "scale1", 
                            return  = "data.frame", 
                            data    = data_example), 
                data.frame(scale1_item1 = c(6, 1, 3, 4, 5, 9, 9),
                           scale1_item2 = c(7, 2, 4, 5, 4, 8, 9),
                           scale1_item3 = c(8, 1, 5, 4, 4, 9, 8)))
+  expect_equal(column_find(pattern = "scale1", 
+                           return  = "data.frame", 
+                           data    = data_example,
+                           invert  = TRUE), 
+               data.frame(scale2_item1 = c(9, 9, 9, 8, 4, 2, 2),
+                          scale2_item2 = c(7, 8, 7, 9, 5, 1, 2)))
   expect_warning(column_find(pattern = "scale1", 
                              return  = "abcdefghijklmnopqrstuvwxyz", 
                              data    = data_example), 
