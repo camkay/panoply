1417a30d14beb77042974dd6e9ae0e7790f53415
Cameron Kay <ckay@uoregon.edu>
2019-07-14 17:53:39 -0700

    Removed additional arguments from simplified text_format functions.

--- R/bold.R
+++ R/bold.R
@@ -3,8 +3,6 @@
 #' A function for easily formatting strings as bold or italicized using markdown or LaTeX syntax. `bold(x)` is a shortcut for `text_format(x, format = "bold", latex = FALSE)` and `italic(x)` is a shortcut for `text_format(x, format = "italic", latex = FALSE)`. `bold_tex(x)` is a shortcut for `text_format(x, format = "bold", latex = TRUE)` and `italic_tex(x)` is a shortcut for `text_format(x, format = "italic", latex = TRUE)`. 
 #' 
 #' @param x a character scalar or vector to be formatted. 
-#' @param format a string specifying whether the text should be boled or italicized. 
-#' @param latex if TRUE, text is formatted using LaTeX formatting. 
 #' @export
 
 bold <- function(x) {
