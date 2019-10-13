07e5701a2922df10d1d73cbab1ac18504b9070c0
Cameron Kay <ckay@uoregon.edu>
2019-10-13 10:20:39 -0700

    Moved all text_format shortcuts into the text_format script.

--- R/text_format.R
+++ R/text_format.R
@@ -45,3 +45,53 @@ text_format <- function(x, format = "italic", latex = FALSE) {
 bold <- function(x) {
   text_format(x, format = "bold", latex = FALSE)
 }
+
+#' @rdname text_format
+#' @export
+
+bold_tex <- function(x) {
+  text_format(x, format = "bold", latex = TRUE)
+}
+
+
+#' @rdname text_format
+#' @export
+
+italic <- function(x) {
+  text_format(x, format = "italic", latex = FALSE)
+}
+
+#' @rdname text_format
+#' @export
+
+italic_tex <- function(x) {
+  text_format(x, format = "italic", latex = TRUE)
+}
+
+#' @rdname text_format
+#' @export
+
+supers <- function(x) {
+  text_format(x, format = "super", latex = FALSE)
+}
+
+#' @rdname text_format
+#' @export
+
+supers_tex <- function(x) {
+  text_format(x, format = "super", latex = TRUE)
+}
+
+#' @rdname text_format
+#' @export
+
+subs <- function(x) {
+  text_format(x, format = "sub", latex = FALSE)
+}
+
+#' @rdname text_format
+#' @export
+
+subs_tex <- function(x) {
+  text_format(x, format = "sub", latex = TRUE)
+}
