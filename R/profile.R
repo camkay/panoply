64150b68ee355fba4411f1ec0c3955e6c63bee68
camkay <ckay@uoregon.edu>
2022-05-26 11:40:31 -0700

    Updated profile to allow for passing arguments to cor.

--- R/profile.R
+++ R/profile.R
@@ -3,6 +3,7 @@
 #' Calculates the profile similarity between two profiles. Currently only supports "de" (double-entry *q* correlations) and "r" (Pearson correlation) methods.
 #' @param data a dataset containing the two columns you wish to conduct a profile similarity analysis on.
 #' @param method a string indicating the type of profile similarity analysis you wish to conduct. Defaults to "de" or double-entry q correlations.
+#' @param ... optional arguments to be passed to cor.
 #' @export
 
 
@@ -20,11 +21,11 @@ profile <- function(data, method = "de") {
   # calculate profile similarity
   if (method == "de") {
     data <- rbind(data, cbind(data[,2], data[,1]))
-    out  <- cor(data[,1], data[,2]) 
+    out  <- cor(data[,1], data[,2], ...) 
   } else if (method == "r") {
     print(data[,1])
     print(data[,2])
-    out <- cor(data[,1], data[,2])
+    out <- cor(data[,1], data[,2], ...)
   }
   
   # out
