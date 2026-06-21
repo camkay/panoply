ab83aee95bcc26ae05ce7bb31a7b8f80921a6522
camkay <ckay@uoregon.edu>
2026-06-21 11:01:13 -0500

    Added fitbit distance badges.

---
+++ R/fitbit_distance_badges.R
@@ -0,0 +1,14 @@
+#' Fitbit Distance Badges
+#'
+#' A dataset including the Fitbit lifetime distance badges. It also includes some additional badges that are not from Fitbit.
+#' 
+#' @format A data frame with 5 rows and 3 variables:
+#' \describe{
+#'   \item{badge}{The name of the lifetime distance badge}
+#'   \item{fitbit}{Whether or not the badge is from Fitbit}
+#'   \item{distances_mi}{The distance corresponding to the badge in miles}
+#'   \item{distances_km}{The distance corresponding to the badge in kilometres}
+#' }
+#'
+
+"fitbit_distance_badges"
